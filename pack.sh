1589fe2cc59f69e543aa8f02ced615b8a186cf5d
Håkan Edling <hakan@tidyui.com>
2020-05-29 08:57:14 +0200

    Simplified pack-script, updated workflow, added solution

--- pack.sh
+++ pack.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env bash
-
-# Define directories
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-OUTPUT_DIR=$SCRIPT_DIR/artifacts
-
-# Clean and build in release
-cd src/Statica
 dotnet restore
 dotnet clean
 dotnet build -c Release
-cd ..
-cd ..
-
-# Make sure output folder exist.
-if [ ! -d "$OUTPUT_DIR" ]; then
-  mkdir "$OUTPUT_DIR"
-fi
-
-# Create all NuGet packages
-dotnet pack src/Statica/Statica.csproj --no-build -c Release -o $OUTPUT_DIR
+dotnet pack --no-build -c Release -o ./artifacts
